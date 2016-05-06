003a9b7bcfb926500b255b75cd2fa27048a70fb1
Renato2012 <renatocavalcante93@hotmail.com>
2016-05-06 19:37:31 -0300

    Correção de possível bug

--- servidor/tomadecisoes.py
+++ servidor/tomadecisoes.py
@@ -5,7 +5,7 @@ import commands
 import time
 
 # Renato
-# 07/08/2014
+# 06/05/2016
 # O servidor chama essa função, para a verficação periodica do status dos clientes para a tomada de decisões.  
 
 # verifica log, se tiver a mensagem Capacidade não é suficiente. Liga nova MF
@@ -17,7 +17,7 @@ def verLog(nomeThread, delay):
         result = os.system("tail /srv/cloud/one/var/sched.log | grep 'Not enough capacity'")
         if result == 0: 	# achou mensagem, chama scprit liga-desliga.sh
             qtd = qtd + 1	# passando a qtd de MFs.
-            os.system("bash ~/cliente/liga-desliga.sh l "+ qtd)
+            os.system("bash ~/cliente/liga-desliga.sh l " + str(qtd))
         time.sleep (delay)	# espera 30 segundos e verifica outra vez.
 
 
@@ -27,10 +27,10 @@ def estado(cliente, msgRecebida):
     nome_host = commands.getoutput("grep "+ ip_cliente +" /etc/hosts | sed -r 's/([[:digit:]]+\.){3}[0-9]+\t//'")
     id_host = commands.getoutput("onehost list | grep "+ nome_host +" | cut -d' ' -f3")
 
-    if msgRecebida == "stop":
-# Antes de desabilitar o host, ver se tem VMs, para migra-las para as MF fixas.
+    if str(msgRecebida) == "stop":
+# Antes de desabilitar o host, ver se tem VMs, para migra-las para as MFs.
         result = commands.getoutput("onevm list -l STAT,HOST | grep "+ nome_host)
-        if result.find('runn') == 0:		# tem VM em estado runn        
+        if "runn" in str(result):		# tem VM em estado runn na MF        
             print "VM status runn, Host: ", nome_host
 # onehost flush desabilita o host e migra VMs para hosts com capacidade de recursos disponiveis.
             os.system("onehost flush "+ nome_host)
@@ -43,4 +43,5 @@ def estado(cliente, msgRecebida):
 # Se msg não for stop, então é para habilitar o host remoto na nuvem.
     else:
         os.system("onehost enable "+ id_host)
+
 
